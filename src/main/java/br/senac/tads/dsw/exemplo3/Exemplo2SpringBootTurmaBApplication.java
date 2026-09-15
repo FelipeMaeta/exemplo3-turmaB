fa188e40f1ad60a57eeae9dc01edcb8dbfcaf276
felipe.msantos59 <maetafelipe@gmail.com>
2026-09-15 09:14:17 -0300

    Refatoração inicial para Exemplo 3

--- src/main/java/br/senac/tads/dsw/exemplo3/Exemplo2SpringBootTurmaBApplication.java
+++ src/main/java/br/senac/tads/dsw/exemplo3/Exemplo2SpringBootTurmaBApplication.java
@@ -1,4 +1,4 @@
-package br.senac.tads.dsw.exemplo2;
+package br.senac.tads.dsw.exemplo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
