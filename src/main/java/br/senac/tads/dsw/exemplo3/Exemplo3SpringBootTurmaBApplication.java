d8c080338cab07354809ea8262ae9d5dff869899
felipe.msantos59 <maetafelipe@gmail.com>
2026-09-15 09:22:19 -0300

    Alteração no nome do arquivo Application

--- src/main/java/br/senac/tads/dsw/exemplo3/Exemplo3SpringBootTurmaBApplication.java
+++ src/main/java/br/senac/tads/dsw/exemplo3/Exemplo3SpringBootTurmaBApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Exemplo2SpringBootTurmaBApplication {
+public class Exemplo3SpringBootTurmaBApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Exemplo2SpringBootTurmaBApplication.class, args);
+		SpringApplication.run(Exemplo3SpringBootTurmaBApplication.class, args);
 	}
 
 }
